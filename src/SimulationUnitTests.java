1f5ca8b2b138d2dce1f77dfaecc59cd00b2f69ee
jev-mcit <jevargas@seas.upenn.edu>
2019-04-20 13:35:12 -0500

    Change name to Junit test file

--- src/SimulationUnitTests.java
+++ src/SimulationUnitTests.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class FutureProjectionTest {
+class SimulationUnitTests {
 
 	@Test
 	void testGetAgeBroke1() {
