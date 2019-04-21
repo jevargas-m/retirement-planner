3b09a44216f89dc9cef89a35d90f442f56aaa29d
arleysasson <49437185+arleysasson@users.noreply.github.com>
2019-04-21 17:55:13 -0400

    Update MonthlyReturn.java

--- src/MonthlyReturn.java
+++ src/MonthlyReturn.java
@@ -1,10 +1,15 @@
-
+/**
+ * Stores returns for stocks and bonds for each month
+ * 
+ * @author Team 11
+ *
+ */
 public class MonthlyReturn {
 	private Double stocksReturn;
 	private Double bondsReturn;
 	
 	
-	//constructor for Flight class that uses most of the data columns from the csv file
+	//constructor for class that uses most of the data columns from the csv file
 	public MonthlyReturn(Double stocksRet, Double bondsRet) {
 		stocksReturn = stocksRet;
 		bondsReturn = bondsRet;
